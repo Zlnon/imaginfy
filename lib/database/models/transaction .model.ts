9c7a83210e2da70e2c7f1c93ca3cf0909048b453
Zlnon <mtarekk666@gmail.com>
2024-03-22 15:57:27 +0300

    added profle and  buy cridits

--- lib/database/models/transaction .model.ts
+++ lib/database/models/transaction .model.ts
@@ -2,8 +2,8 @@ import { Schema, model, models } from "mongoose";
 
 const TransactionSchema = new Schema({
   createdAt: { type: Date, default: Date.now },
-  stripedId: { Type: String, requied: true, unique: true },
-  amount: { type: Number, reqired: true },
+  stripedId: { type: String, required: true, unique: true },
+  amount: { type: Number, required: true },
   plan: { type: String },
   credits: { type: Number },
   buyer: { type: Schema.Types.ObjectId, ref: "User" },
